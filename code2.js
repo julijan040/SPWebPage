0ddcf8d29f111b85994d0eb0afd96a2db7c92671
julijan040 <js1840@student.uni-lj.si>
2015-11-22 16:06:28 +0100

    More functionality

--- code2.js
+++ code2.js
@@ -1,8 +1,33 @@
-$(document).ready(function() {
-    $("#inputPostOrSearch").maxlength({max: 100});
-});
+var full = false;
 
 function textAreaAdjust(o) {
     o.style.height = "1px";
     o.style.height = (25+o.scrollHeight)+"px";
 }
+
+
+
+window.setInterval(function(){
+
+    var enteredText = $("#inputPostOrSearch").val();
+    var numberOfLineBreaks = (enteredText.match(/\n/g)||[]).length;
+    var characterCount = enteredText.length + numberOfLineBreaks;
+
+    if(characterCount == 50 && !full)
+    {
+        $('#saySomething').prepend(
+            '<img class="img-responsive" src="sprites/maxLength.png" alt="Chania" id="maxLength">'
+        );
+
+        full = true;
+    }
+    else if(characterCount<50)
+    {
+
+        $('#maxLength').remove();
+
+        full = false;
+
+    }
+
+}, 100);
