658b3d462737eef01ff53e7a2eefa9090e92cfc7
julijan040 <js1840@student.uni-lj.si>
2015-11-22 21:47:52 +0100

    Commit

---
+++ code.js
@@ -0,0 +1,45 @@
+application/x-javascript
+
+$(document).ready(function() {
+
+    //sliding navigation
+    $('a[href*=#]').each(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+            && location.hostname == this.hostname
+            && this.hash.replace(/#/,'') ) {
+            var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+            var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+            if ($target) {
+                var targetOffset = $target.offset().top;
+
+            //move
+                $(this).click(function() {
+                    $("#nav li a").removeClass("active");
+                    $(this).addClass('active');
+                    $('html, body').animate({scrollTop: targetOffset}, 1000);
+                    return false;
+                });
+            }
+        }
+    });
+
+    $('.form-control').keypress(function() {
+
+        var textLength = $(this).val().length;
+
+        if(textLength < 20) {
+            // Do noting
+        } else if (textLength < 40) {
+            $(this).css('font-size', '85%');
+        } else if (textLength > 40) {
+            $(this).css('font-size', '70%');
+        }
+    });
+
+
+
+});
+
+$(window).on('beforeunload', function(){
+    $(window).scrollTop(0);
+});
---
+++ code2.js
@@ -0,0 +1,93 @@
+
+var full = false;
+var canChangeProfPictue = true;
+var canAddPictureToPost = true;
+
+function textAreaAdjust(o) {
+    o.style.height = "1px";
+    o.style.height = (25+o.scrollHeight)+"px";
+}
+$(document).ready(function() {
+
+    //for detecting errors in pictures For Profile picture
+    $("#changePic").change(function() {
+
+        var val = $(this).val();
+
+        switch(val.substring(val.lastIndexOf('.') + 1).toLowerCase()){
+            case 'gif': case 'jpg': case 'png':
+            if(!canChangeProfPictue)
+            {
+                $('#cantChangeProfPic').remove();
+            }
+            //let changing of ProfilePictureBegin
+            break;
+            default:
+                $(this).val('');
+                if(canChangeProfPictue)
+                {
+                    $('#chanePicDiv1').append(
+                        '<img class="img-responsive" src="sprites/notAnImage.png" alt="Chania" id="cantChangeProfPic">'
+                    );
+                    canChangeProfPictue=false;
+                }
+
+
+                break;
+        }
+    });
+
+    //for detecting errors in pictures For Post picture
+    $("#Attach").change(function() {
+
+        var val = $(this).val();
+
+        switch(val.substring(val.lastIndexOf('.') + 1).toLowerCase()){
+            case 'gif': case 'jpg': case 'png':
+            if(!canAddPictureToPost)
+            {
+                $('#cantChangePostPic').remove();
+            }
+            //let changing of ProfilePictureBegin
+            break;
+            default:
+                $(this).val('');
+                if(canAddPictureToPost)
+                {
+                    $('#postAttachDiv').append(
+                        '<img class="img-responsive" src="sprites/notAnImage.png" alt="Chania" id="cantChangePostPic">'
+                    );
+                    canAddPictureToPost=false;
+                }
+
+
+                break;
+        }
+    });
+
+});
+
+window.setInterval(function(){
+
+    var enteredText = $("#inputPostOrSearch").val();
+    var numberOfLineBreaks = (enteredText.match(/\n/g)||[]).length;
+    var characterCount = enteredText.length + numberOfLineBreaks;
+
+    if(characterCount == 250 && !full)
+    {
+        $('#saySomething').prepend(
+            '<img class="img-responsive" src="sprites/maxLength.png" alt="Chania" id="maxLength">'
+        );
+
+        full = true;
+    }
+    else if(characterCount<250)
+    {
+
+        $('#maxLength').remove();
+
+        full = false;
+
+    }
+
+}, 100);
