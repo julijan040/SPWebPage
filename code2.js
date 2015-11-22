723b901227f94a37af8db0b9513439779697b9a8
julijan040 <js1840@student.uni-lj.si>
2015-11-22 18:07:37 +0100

    Almost Done WebPage

--- code2.js
+++ code2.js
@@ -1,11 +1,70 @@
 var full = false;
+var canChangeProfPictue = true;
+var canAddPictureToPost = true;
 
 function textAreaAdjust(o) {
     o.style.height = "1px";
     o.style.height = (25+o.scrollHeight)+"px";
 }
+$(document).ready(function() {
+
+    //for detecting errors in pictures For Profile picture
+    $("#changePic").change(function() {
+
+        var val = $(this).val();
+
+        switch(val.substring(val.lastIndexOf('.') + 1).toLowerCase()){
+            case 'gif': case 'jpg': case 'png':
+            if(!canChangeProfPictue)
+            {
+                $('#cantChangeProfPic').remove();
+            }
+            //let changing of ProfilePictureBegin
+            break;
+            default:
+                $(this).val('');
+                if(canChangeProfPictue)
+                {
+                    $('#chanePicDiv1').append(
+                        '<img class="img-responsive" src="sprites/notAnImage.png" alt="Chania" id="cantChangeProfPic">'
+                    );
+                    canChangeProfPictue=false;
+                }
 
 
+                break;
+        }
+    });
+
+    //for detecting errors in pictures For Post picture
+    $("#Attach").change(function() {
+
+        var val = $(this).val();
+
+        switch(val.substring(val.lastIndexOf('.') + 1).toLowerCase()){
+            case 'gif': case 'jpg': case 'png':
+            if(!canAddPictureToPost)
+            {
+                $('#cantChangePostPic').remove();
+            }
+            //let changing of ProfilePictureBegin
+            break;
+            default:
+                $(this).val('');
+                if(canAddPictureToPost)
+                {
+                    $('#postAttachDiv').append(
+                        '<img class="img-responsive" src="sprites/notAnImage.png" alt="Chania" id="cantChangePostPic">'
+                    );
+                    canAddPictureToPost=false;
+                }
+
+
+                break;
+        }
+    });
+
+});
 
 window.setInterval(function(){
 
@@ -13,7 +72,7 @@ window.setInterval(function(){
     var numberOfLineBreaks = (enteredText.match(/\n/g)||[]).length;
     var characterCount = enteredText.length + numberOfLineBreaks;
 
-    if(characterCount == 50 && !full)
+    if(characterCount == 250 && !full)
     {
         $('#saySomething').prepend(
             '<img class="img-responsive" src="sprites/maxLength.png" alt="Chania" id="maxLength">'
@@ -21,7 +80,7 @@ window.setInterval(function(){
 
         full = true;
     }
-    else if(characterCount<50)
+    else if(characterCount<250)
     {
 
         $('#maxLength').remove();
